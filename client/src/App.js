432f0dccf2e47db7519952a8800946257602acad
Julius Casipit <52521862+juggernautSmash@users.noreply.github.com>
2020-01-18 14:23:19 -0800

    installed material ui, fixed client folder structure, render a simple navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-
+import React from 'react'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div>
+        <NavBar />
         Hello World!!!
     </div>
   );
 }
 
-export default App;
+export default App
