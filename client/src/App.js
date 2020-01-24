ee97d46e67c386de2bc094fd1515f2efbab26b33
caseyjames95 <caseypurves@gmail.com>
2020-01-23 20:40:39 -0800

    AppInfo displays created

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import FooterBar from './components/FooterBar'
-import CompanyForm from '../src/components/CompanyForm'
 
 function App() {
   return (
     <div>
         <NavBar />
         Hello World!!!
-
         <FooterBar />
     </div>
   );
