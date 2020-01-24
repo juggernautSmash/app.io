52e95df85efd0c499107d4ad94661c11f5c9f20a
caseyjames95 <caseypurves@gmail.com>
2020-01-23 19:15:25 -0800

    Dumb form
    Basic form layoput.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import FooterBar from './components/FooterBar'
+import CompanyForm from '../src/components/CompanyForm'
 
 function App() {
   return (
     <div>
         <NavBar />
         Hello World!!!
+
         <FooterBar />
     </div>
   );
--- client/src/components/CompanyForm/CompanyForm.js
+++ client/src/components/CompanyForm/CompanyForm.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import './CompanyForm.css'
+import { FormControl, InputLabel, Input, FormHelperText, Button } from '@material-ui/core'
+
 
 const CompanyForm = _ => {
     return (
-        <>
-        </>
+        <form id="CFS">
+            <h2>Company Sign-Up</h2>
+            <p>
+                <FormControl>
+                    <InputLabel htmlFor="my-input">Company Name</InputLabel>
+                    <Input id="my-input" aria-describedby="my-helper-text" />
+                    <FormHelperText id="my-helper-text"></FormHelperText>
+                </FormControl>
+            </p>
+            <p>
+                <FormControl>
+                    <InputLabel htmlFor="my-input">Address</InputLabel>
+                    <Input id="my-input" aria-describedby="my-helper-text" />
+                    <FormHelperText id="my-helper-text"></FormHelperText>
+                </FormControl>
+            </p>
+            <p>
+                <FormControl>
+                    <InputLabel htmlFor="my-input">Company Contact</InputLabel>
+                    <Input id="my-input" aria-describedby="my-helper-text" />
+                    <FormHelperText id="my-helper-text">Email</FormHelperText>
+                </FormControl>
+            </p>
+            <p>
+                <FormControl>
+                    <InputLabel htmlFor="my-input">Employee Emails</InputLabel>
+                    <Input id="my-input" aria-describedby="my-helper-text" />
+                    <FormHelperText id="my-helper-text"></FormHelperText>
+                </FormControl>
+            </p>
+            <p>
+                <Button>
+                    Submit
+                </Button>
+            </p>
+        </form>
     )
 }
 
