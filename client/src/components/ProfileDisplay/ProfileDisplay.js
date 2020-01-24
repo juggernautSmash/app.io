da14d45d3bede3e456b0365ccfc0894159c89ee6
rsum1 <ryanjsumpter+github@gmail.com>
2020-01-23 20:51:40 -0800

    basic profile display

---
+++ client/src/components/ProfileDisplay/ProfileDisplay.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+
+
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+    height: '90px',
+    width: '90px'
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  attr: {
+    marginTop: theme.spacing(1),
+    fontSize: 16
+  },
+}));
+
+const ProfileDisplay = () => {
+  const classes = useStyles();
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          {/* add photo here */}
+          userPic
+        </Avatar>
+      </div>
+      <div className = {classes.form}>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <Typography color="textSecondary" variant="h5" className={classes.attr}>
+              Name
+        </Typography>
+            <Typography color="textSecondary" variant="h5" className={classes.attr}>
+              Title
+        </Typography>
+            <Typography color="textSecondary" variant="h5" className={classes.attr}>
+              Email
+        </Typography>
+            <Typography color="textSecondary" variant="h5" className={classes.attr}>
+              Phone
+        </Typography>
+            <Typography color="textSecondary" variant="h5" className={classes.attr}>
+              Location
+        </Typography>
+            <Typography color="textSecondary" variant="h5" className={classes.attr}>
+              TimeZone
+        </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="h5" className={classes.attr}>
+              John Wick
+        </Typography>
+            <Typography variant="h5" className={classes.attr}>
+              Assassin
+        </Typography>
+            <Typography variant="h5" className={classes.attr}>
+              John.Wick@gmail.com
+        </Typography>
+            <Typography variant="h5" className={classes.attr}>
+              (xxx) xxx-xxxx
+        </Typography>
+            <Typography variant="h5" className={classes.attr}>
+              Unknown
+        </Typography>
+            <Typography variant="h5" className={classes.attr}>
+              PST
+        </Typography>
+          </Grid>
+        </Grid>
+      </div>
+    </Container>
+  );
+}
+// const SignUpForm = () => {
+//   return (
+//     <div>
+//       THis is the signUpForm
+//     </div>
+//   )
+// }
+
+
+
+export default ProfileDisplay
