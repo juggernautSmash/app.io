9cf90e12ace2dabd10cc19919a7607316fbc6abe
caseyjames95 <caseypurves@gmail.com>
2020-01-25 11:13:45 -0800

    Remove tests from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import FooterBar from './components/FooterBar'
-import CompanyInfo from './components/CompanyInfo'
 
 function App() {
   return (
     <div>
         <NavBar />
         Hello World!!!
-        <CompanyInfo />
         <FooterBar />
     </div>
   );
