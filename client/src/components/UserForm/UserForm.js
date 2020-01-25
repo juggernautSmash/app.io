fbb77cb09152faea1771ef8c4524ae377707292e
Derek Smith <dsmith0789@gmail.com>
2020-01-25 10:48:47 -0800

    update to UserForm and connection to firebase

--- client/src/components/UserForm/UserForm.js
+++ client/src/components/UserForm/UserForm.js
@@ -2,6 +2,29 @@ import React from 'react'
 import './UserForm.css'
 import { FormControl, InputLabel, Input, FormHelperText, Button } from '@material-ui/core'
 
+<script src="https://www.gstatic.com/firebasejs/7.7.0/firebase-app.js"></script>
+
+<script src="https://www.gstatic.com/firebasejs/7.7.0/firebase-analytics.js"></script>
+
+<script>
+
+        // Your web app's Firebase configuration
+        var firebaseConfig = {
+            apiKey: "AIzaSyBApRnJ1RfuBN8T7xLi2jeDF19v3VDfkEY",
+    authDomain: "app-io-f6252.firebaseapp.com",
+    databaseURL: "https://app-io-f6252.firebaseio.com",
+    projectId: "app-io-f6252",
+    storageBucket: "app-io-f6252.appspot.com",
+    messagingSenderId: "436155669423",
+    appId: "1:436155669423:web:c92be876535e3eb73d2d5a",
+    measurementId: "G-PEVS3L4YPC"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+</script>
+
 
 const CompanyForm = _ => {
     return (
@@ -25,7 +48,7 @@ const CompanyForm = _ => {
                 <FormControl>
                     <InputLabel htmlFor="my-input">Position</InputLabel>
                     <Input id="my-input" aria-describedby="my-helper-text" />
-                    <FormHelperText id="my-helper-text">Email</FormHelperText>
+                    <FormHelperText id="my-helper-text"></FormHelperText>
                 </FormControl>
             </p>
             <p>
