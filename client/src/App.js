071126dbcd4f3b34430e4132c652302487d257da
Julius Casipit <52521862+juggernautSmash@users.noreply.github.com>
2020-01-27 18:54:18 -0800

    removed the pages folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,15 @@
 import React from 'react'
-import NavBarPage from './pages/NavBarPage'
-import FooterBar from './components/FooterBar'
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import firebase from 'firebase'
 
+import FooterBar from './components/FooterBar'
 import LandingPage from './components/LandingPage'
+
+import NavBarPage from './pages/NavBarPage'
 import SignUpPage from './pages/SignUpPage'
-import CompanySignUpPage from './pages/CompanySignUpPage'
-import UserSignUpPage from './pages/UserSignUpPage'
 import LoginPage from './pages/LoginPage'
-import UserLoginPage from './pages/UserLoginPage'
-import CompanyLogin from './components/CompanyLogin'
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
-
-  // class SignInScreen extends React.Component {
-  //   render() {
-  //     return (
-  //       <div>
-  //         <h1>My App</h1>
-  //         <p>Please sign-in:</p>
-  //         <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-  //       </div>
-  //     );
-  //   }
-  // }
-
-
 
   return (
     <BrowserRouter>
