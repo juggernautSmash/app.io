78626379a05fd1aa5509d6e220e15416894f1aa7
Derek Smith <dsmith0789@gmail.com>
2020-01-30 20:35:35 -0800

    Tools to setup google login

--- client/src/components/GoogleTestLogin/GoogleTestLogin.js
+++ client/src/components/GoogleTestLogin/GoogleTestLogin.js
@@ -1,65 +1,62 @@
-// Import FirebaseAuth and firebase.
-import React from 'react';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import firebase from 'firebase';
+// import { NgbAuthFirebaseUIModule } from '@firebaseui/ng-bootstrap';
+
+// @NgModule({
+//   declarations: [AppComponent, ...],
+//   imports: [NgbAuthFirebaseUIModule.forRoot({
+//     apiKey: 'your-firebase-apiKey',
+//     authDomain: 'your-firebase-authDomain',
+//     databaseURL: 'your-firebase-databaseURL',
+//     projectId: 'your-firebase-projectId',
+//     storageBucket: 'your-firebase-storageBucket',
+//     messagingSenderId: 'your-firebase-messagingSenderId'
+//   }), ...],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule {
+// }
+
+
+// Other modules in your application can simply import NgbAuthFirebaseUIModule: 
+
+// import { NgbAuthFirebaseUIModule } from '@firebaseui/ng-bootstrap';
+
+// @NgModule({
+//   declarations: [OtherComponent, ...],
+//   imports: [NgbAuthFirebaseUIModule, ...], 
+// })
+// export class OtherModule {
+// }
+
+
+// <!-- You can now use the library component in app.component.html  -->
  
-// Configure Firebase.
-const config = {
-    apiKey: "AIzaSyBApRnJ1RfuBN8T7xLi2jeDF19v3VDfkEY",
-    authDomain: "app-io-f6252.firebaseapp.com",
-  // ...
-};
-firebase.initializeApp(config);
+// <ngb-auth-firebaseui (onSuccess)="printUser($event)"
+//                      (onError)="printError()">
+// </ngb-auth-firebaseui>
+
+// How to disable users to sign in and continue as guest, use the guestEnabled input
+// <ngb-auth-firebaseui [guestEnabled]="false"
+//                      (onSuccess)="printUser($event)"
+//                      (onError)="printError($event)">
+// </ngb-auth-firebaseui>
+
+// e.g: in your component, import the AuthProvider enum to pick up your favorite provider:
+
+// import {OnInit} from '@angular/core';
+// import {AuthProvider} from '@firebaseui/ng-bootstrap';
  
-class SignInScreen extends React.Component {
+// export class ExampleComponent implements OnInit {
  
-  // The component's Local state.
-  state = {
-    isSignedIn: false // Local signed-in state.
-  };
+//   providers = AuthProvider;
  
-  // Configure FirebaseUI.
-  uiConfig = {
-    // Popup signin flow rather than redirect flow.
-    signInFlow: 'popup',
-    // We will display Google and Facebook as auth providers.
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID
-    ],
-    callbacks: {
-      // Avoid redirects after sign-in.
-      signInSuccessWithAuthResult: () => false
-    }
-  };
- 
-  // Listen to the Firebase Auth state and set the local state.
-  componentDidMount() {
-    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
-        (user) => this.setState({isSignedIn: !!user})
-    );
-  }
-  
-  // Make sure we un-register Firebase observers when the component unmounts.
-  componentWillUnmount() {
-    this.unregisterAuthObserver();
-  }
- 
-  render() {
-    if (!this.state.isSignedIn) {
-      return (
-        <div>
-          <h1>My App</h1>
-          <p>Please sign-in:</p>
-          <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
-        </div>
-      );
-    }
-    return (
-      <div>
-        <h1>My App</h1>
-        <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
-        <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
-      </div>
-    );
-  }
-}
+//   ngOnInit() {
+//   }
+// }
+
+
+// in your template -->
+
+//   <ngb-auth-firebaseui
+//   [providers]="[providers.Google]"
+//   (onSuccess)="printUser($event)"
+//    (onError)="printError()"></ngb-auth-firebaseui>
