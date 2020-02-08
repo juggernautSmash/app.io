82f48adc50c69c50b97361085f67e2cd76e167dc
Julius Casipit <52521862+juggernautSmash@users.noreply.github.com>
2020-02-08 11:13:44 -0800

    changed "id" to "className" for modalButton

--- client/src/components/LoginModal/LoginModal.js
+++ client/src/components/LoginModal/LoginModal.js
@@ -36,7 +36,7 @@ export default function LoginModal() {
 
   return (
     <div>
-      <Button id="modalButton" type="button" onClick={handleOpen}>
+      <Button className="modalButton" type="button" onClick={handleOpen}>
         Login
       </Button>
       <Modal
