6b7a18197bdbd6069cde77146f9d1f1301535985
caseyjames95 <caseypurves@gmail.com>
2020-02-13 19:24:19 -0800

    Links to menu

--- client/src/components/HamMenu/HamMenu.js
+++ client/src/components/HamMenu/HamMenu.js
@@ -5,128 +5,112 @@ import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-
+import { Link } from 'react-router-dom'
 // Icons
 import MenuIcon from '@material-ui/icons/Menu';
 import PersonIcon from '@material-ui/icons/Person';
 import GroupIcon from '@material-ui/icons/Group';
-import EventAvailableIcon from '@material-ui/icons/EventAvailable';
 import AssignmentIcon from '@material-ui/icons/Assignment';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import SettingsIcon from '@material-ui/icons/Settings';
-import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
+import Context from '../../utils/Context'
+
 const StyledMenu = withStyles({
-  paper: {
-    border: '1px solid #d3d4d5',
-  },
+    paper: {
+        border: '1px solid #d3d4d5',
+    },
 })(props => (
-  <Menu
-    elevation={0}
-    getContentAnchorEl={null}
-    anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'center',
-    }}
-    transformOrigin={{
-      vertical: 'top',
-      horizontal: 'center',
-    }}
-    {...props}
-  />
+    <Menu
+        elevation={0}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+        }}
+        transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+        }}
+        {...props}
+    />
 ));
 
 const StyledMenuItem = withStyles(theme => ({
-  root: {
-    '&:focus': {
-      backgroundColor: theme.palette.primary.main,
-      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
-        color: theme.palette.common.white,
-      },
+    root: {
+        '&:focus': {
+            backgroundColor: theme.palette.primary.main,
+            '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+                color: theme.palette.common.white,
+            },
+        },
     },
-  },
 }))(MenuItem);
 
 export default function HamMenu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+    const [anchorEl, setAnchorEl] = React.useState(null);
 
-  const handleClick = event => {
-    setAnchorEl(event.currentTarget);
-  };
+    const { logout } = React.useContext(Context)
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
 
-  return (
-    <div>
-      <MenuIcon
-        fontSize="large"
-        aria-controls="customized-menu"
-        aria-haspopup="true"
-        // variant="contained"
-        // color="primary"
-        onClick={handleClick}
-      >
-        Open Menu
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div>
+            <MenuIcon
+                fontSize="large"
+                aria-controls="customized-menu"
+                aria-haspopup="true"
+                // variant="contained"
+                // color="primary"
+                onClick={handleClick}
+            >
+                Open Menu
       </MenuIcon>
-      <StyledMenu
-        id="customized-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <StyledMenuItem>
-          <ListItemIcon>
-            <PersonIcon fontSize="medium" />
-          </ListItemIcon>
-          <ListItemText primary="Profile" />
-        </StyledMenuItem>
-        <StyledMenuItem>
-          <ListItemIcon>
-            <GroupIcon fontSize="medium" />
-          </ListItemIcon>
-          <ListItemText primary="Team" />
-        </StyledMenuItem>
-        <StyledMenuItem>
-          <ListItemIcon>
-            <EventAvailableIcon fontSize="medium" />
-          </ListItemIcon>
-          <ListItemText primary="Planner" />
-        </StyledMenuItem>
-        <StyledMenuItem>
-          <ListItemIcon>
-            <AssignmentIcon fontSize="medium" />
-          </ListItemIcon>
-          <ListItemText primary="Boards" />
-        </StyledMenuItem>
-        <StyledMenuItem>
-          <ListItemIcon>
-            <DashboardIcon fontSize="medium" />
-          </ListItemIcon>
-          <ListItemText primary="Dashboard" />
-        </StyledMenuItem>
-        <StyledMenuItem>
-          <ListItemIcon>
-            <SettingsIcon fontSize="medium" />
-          </ListItemIcon>
-          <ListItemText primary="Settings" />
-        </StyledMenuItem>
-        <StyledMenuItem>
-          <ListItemIcon>
-            <PersonAddIcon fontSize="medium" />
-          </ListItemIcon>
-          <ListItemText primary="Invite Members" />
-        </StyledMenuItem>
-        <StyledMenuItem>
-          <ListItemIcon>
-            <ExitToAppIcon fontSize="medium" />
-          </ListItemIcon>
-          <ListItemText primary="Logout" />
-        </StyledMenuItem>
-      </StyledMenu>
-    </div>
-  );
+            <StyledMenu
+                id="customized-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <Link to={'/user'}>
+                    <StyledMenuItem onClick={_ => <Link to="/user" />}>
+                        <ListItemIcon>
+                            <PersonIcon fontSize="medium" />
+                        </ListItemIcon>
+                        <ListItemText primary="Profile" />
+                    </StyledMenuItem>
+                </Link>
+                <Link to={'/team'}>
+                    <StyledMenuItem>
+                        <ListItemIcon>
+                            <GroupIcon fontSize="medium" />
+                        </ListItemIcon>
+                        <ListItemText primary="Team" />
+                    </StyledMenuItem>
+                </Link>
+                <Link to={'/boards'}>
+                    <StyledMenuItem>
+                        <ListItemIcon>
+                            <AssignmentIcon fontSize="medium" />
+                        </ListItemIcon>
+                        <ListItemText primary="Boards" />
+                    </StyledMenuItem>
+                </Link>
+                <Link to={'/'}>
+                    <StyledMenuItem>
+                        <ListItemIcon>
+                            <ExitToAppIcon onClick = {logout} fontSize="medium" />
+                        </ListItemIcon>
+                        <ListItemText primary="Logout" />
+                    </StyledMenuItem>
+                </Link>
+            </StyledMenu>
+        </div>
+    );
 }
